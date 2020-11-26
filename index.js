b8a5e74c4ae58423cada81cc55b3fd07463d6576
Amex-Abiy <amexabiy@gmail.com>
2020-11-26 14:50:33 +0300

    scrapped some groups

--- index.js
+++ index.js
@@ -4,13 +4,49 @@ const Telegram = require('./telegram')
 
 const BASE_URL = 'https://web.telegram.org/#/login';
 const contryCode = 251;
-const phoneNo = 966245238;
+const phoneNo = 901121642;
 
-const url = 'https://web.telegram.org/#/im?p=@ethio_amazon_group2';
+const urls = [
+    // 'https://web.telegram.org/#/im?p=@ethio_amazon_group2',
+    // 'https://web.telegram.org/#/im?p=@machesmarket',
+    // 'https://web.telegram.org/#/im?p=@ethio_Amazon_group',
+    // 'https://web.telegram.org/#/im?p=@python_amharic',
+    // 'https://web.telegram.org/#/im?p=@Utubers_SubToSub',
+    // 'https://web.telegram.org/#/im?p=@aaititsc',
+    // 'https://web.telegram.org/#/im?p=@flutter_forum',
+    // 'https://web.telegram.org/#/im?p=@ethio_amazon_group3',
+    // 'https://web.telegram.org/#/im?p=@merkatosell',
+    // 'https://web.telegram.org/#/im?p=@walia_online_market',
+    // 'https://web.telegram.org/#/im?p=@guramaylepics'
+    'https://web.telegram.org/#/im?p=@zembilshoppingcenter',
+    'https://web.telegram.org/#/im?p=@ethiotech_discussion',
+    'https://web.telegram.org/#/im?p=@GDG_Addis',
+    'https://web.telegram.org/#/im?p=@christiantshirt',
+    'https://web.telegram.org/#/im?p=@ethio_developer_jobs',
+    'https://web.telegram.org/#/im?p=@Almas_TechG',
+    'https://web.telegram.org/#/im?p=@AIESEC_ExternalRelations',
+    'https://web.telegram.org/#/im?p=@myfellow',
+    'https://web.telegram.org/#/im?p=@etdevs',
+    'https://web.telegram.org/#/im?p=@gofermarket',
+    'https://web.telegram.org/#/im?p=@Shop_Habesha',
+    'https://web.telegram.org/#/im?p=@MerkatoGebeya2',
+    'https://web.telegram.org/#/im?p=@merkatoo1',
+    'https://web.telegram.org/#/im?p=@mezoMarket',
+    'https://web.telegram.org/#/im?p=@merkaton1',
+    'https://web.telegram.org/#/im?p=@ahadumarket12',
+    'https://web.telegram.org/#/im?p=@Zenation_talking',
+    'https://web.telegram.org/#/im?p=@ZionHOTSale',
+    'https://web.telegram.org/#/im?p=@merkato101',
+
+];
 
 (async() => {
-    const telegram = new Telegram(url);
+    const telegram = new Telegram(urls);
     await telegram.initialize(BASE_URL);
     await telegram.login(contryCode, phoneNo);
-    await telegram.scrapeUsers();
+    // const authStatus = await telegram.login(contryCode, phoneNo);
+    // console.log('authStatus -> ', authStatus)
+    // if (authStatus) {
+    //     await telegram.scrapeUsers();
+    // }
 })()
--- telegram.js
+++ telegram.js
@@ -1,4 +1,7 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
+const select = require('puppeteer-select');
+const fs = require('fs');
 
 class Telegram {
     constructor(channelURI) {
@@ -39,31 +42,77 @@ class Telegram {
         await this.page.click('body > div.modal.fade.confirm_modal_window.in > div.modal-dialog > div > div > div.md_simple_modal_footer > button.btn.btn-md.btn-md-primary')
 
         // type in verification code through terminal
-        await this.page.waitForSelector('body > div.page_wrap > div > div.login_page > div.login_form_wrap > form > div.md-input-group.md-input-group-centered.md-input-animated > input')
         const readline = require('readline').createInterface({
             input: process.stdin,
             output: process.stdout
         });
         readline.question('Enter the verification code sent to you - ', async(verificationCode) => {
             console.log(`Verification Code - ${verificationCode}`);
+            readline.close()
             await this.page.type('body > div.page_wrap > div > div.login_page > div.login_form_wrap > form > div.md-input-group.md-input-group-centered.md-input-animated > input', verificationCode.toString(), { delay: 25 })
             await this.page.keyboard.press('Enter');
+            this.scrapeUsers();
         });
-        readline.close()
-
     }
 
     async scrapeUsers() {
-        await this.page.goto(this.channelURI)
-        await this.page.waitForSelector('body > div.page_wrap > div:nth-child(1) > div > div > div.tg_head_main_wrap > div > div.tg_head_peer_title_wrap > a', { timeout: 90000 })
-        await this.page.click('body > div.page_wrap > div:nth-child(1) > div > div > div.tg_head_main_wrap > div > div.tg_head_peer_title_wrap > a')
-        await this.page.waitForSelector('body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div', { timeout: 90000 })
-        let list = document.querySelector('body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div').children()
-        console.log(list.length)
-        await this.page.click('body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div > div:nth-child(5) > div.md_modal_list_peer_name > a')
-            // for (let i = 0; i < list.length; i++) {
+        console.log('this.channelURI', this.channelURI)
 
-        // }
+        // attach arrayUsers to json file containing users fetched so far
+        let arrayUsers = require('./guramaylepics.json').users;
+
+        for (let url = 0; url < this.channelURI.length; url++) {
+            console.log('this.channelURI', this.channelURI[url])
+            await this.page.waitForSelector('body > div.page_wrap > div:nth-child(1) > div > div > div.tg_head_logo_wrap > div > a > div', { timeout: 90000 })
+            await this.page.goto(this.channelURI[url])
+            await this.page.waitForSelector('body > div.page_wrap > div:nth-child(1) > div > div > div.tg_head_main_wrap > div > div.tg_head_peer_title_wrap > a', { timeout: 90000 })
+            await this.page.click('body > div.page_wrap > div:nth-child(1) > div > div > div.tg_head_main_wrap > div > div.tg_head_peer_title_wrap > a')
+            await this.page.waitForSelector('body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div', { timeout: 90000 })
+
+            for (let x = 1; x <= 200; x++) {
+                try {
+                    await this.page.waitForSelector(`body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_head > div.md_modal_title_wrap > div.md_modal_title`)
+                    await this.page.waitForSelector(`body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div > div:nth-child(${x}) > div.md_modal_list_peer_name > a`)
+                    await this.page.click(`body > div.modal.fade.chat_modal_window.channel_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div:nth-child(2) > div.md_modal_iconed_section_wrap.md_modal_iconed_section_peers > div > div:nth-child(${x}) > div.md_modal_list_peer_name > a`)
+                    await this.page.waitForSelector(`body > div.modal.fade.user_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_body > div > div.md_modal_iconed_section_wrap.md_modal_iconed_section_toggle > a > span.tg_checkbox_label`)
+                    let user = await this.page.evaluate(() => {
+                        return {
+                            name: document.querySelector('span[ng-bind*="@"]') ? document.querySelector('span[ng-bind*="@"]').innerText : null
+                        }
+                    });
+                    if (!user.name) {
+                        await this.page.click('body > div.modal.fade.user_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_head > div.md_modal_title_wrap > div.md_modal_actions_wrap.clearfix > a.md_modal_action.md_modal_action_close')
+                        continue;
+                    }
+
+                    console.log(user);
+                    if (arrayUsers.includes(user.name)) {
+                        await this.page.click('body > div.modal.fade.user_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_head > div.md_modal_title_wrap > div.md_modal_actions_wrap.clearfix > a.md_modal_action.md_modal_action_close')
+                        continue;
+                    }
+                    arrayUsers.push(user.name)
+
+                    // CLOSE USER-ACCOUNT POPUP
+                    await this.page.waitForSelector(`body > div.modal.fade.user_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_head > div.md_modal_title_wrap > div.md_modal_actions_wrap.clearfix > a.md_modal_action.md_modal_action_close`)
+                    await this.page.click('body > div.modal.fade.user_modal_window.mobile_modal.in > div.modal-dialog > div > div > div.md_modal_head > div.md_modal_title_wrap > div.md_modal_actions_wrap.clearfix > a.md_modal_action.md_modal_action_close')
+                    console.log(`Done with ${x}`)
+                    console.log(arrayUsers.length)
+
+                } catch (error) {
+                    x--;
+                    continue;
+                }
+            }
+            const users = {
+                users: arrayUsers
+            }
+            fs.writeFile(`./${this.channelURI[url].split('@')[1]}.json`, JSON.stringify(users), 'utf-8', function(err) {
+                if (err) throw err;
+                console.log('complete');
+            })
+        }
+        console.log('list', arrayUsers.length)
+        console.log('list', arrayUsers)
     }
 }
 
